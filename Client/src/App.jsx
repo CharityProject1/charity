4958d8bd76b99d1c1b6b515ea7c40e50c7a2d8bc
AhmadALJabareen <ahmad.aljabareen20@gmail.com>
2025-03-02 15:23:28 +0300

    done ahmad

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,40 +1,40 @@
-
 // import { Router } from 'react-router-dom';
-import AboutUs from './pages/aboutUs/AboutUs'
-import DonationForm from './pages/donations/dd';
-import { Router, Routes, Route } from 'react-router-dom';
-import Donate from './pages/donations/Donate';
-import Request from './pages/requests/Request';
-import RequestForm from './pages/requests/RequestForm';
-import Routedd from './pages/routedashboard/Routedd';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/home/Home';
-import Routedoner from './pages/routedashboard/Routedoners';
-import DonationSection from './pages/donations/card';
-import PaymentPage from './pages/donations/payment';
-import Requestcard from './pages/donations/requests';
+import AboutUs from "./pages/aboutUs/AboutUs";
+// import DonationForm from './pages/donations/dd';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Donate from "./pages/donations/Donate";
+import Request from "./pages/requests/Request";
+import RequestForm from "./pages/requests/RequestForm";
+import Routedd from "./pages/routedashboard/Routedd";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/home/Home";
+import Routedoner from "./pages/routedashboard/Routedoners";
+import DonationSection from "./pages/donations/card";
+import PaymentPage from "./pages/donations/payment";
+import Requestcard from "./pages/donations/requests";
 
 function App() {
-  
-
   return (
     <>
-  {/* 
-  <Routes>
+      <Router>
+        <Navbar />
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/aboutUs" element={<AboutUs />} />
+          <Route path="/test" element={<Requestcard />} />
         </Routes>
-   */}
-   {/* <Routedoner/> */}
-   <Navbar/>
-   <DonationSection/>
-   {/* <Requestcard/> */}
-   <Footer/>
-    {/* <Routedd/> */}
-    {/* <PaymentPage/> */}
- </>
-  )
+        <Footer />
+      </Router>
+
+      {/* <Routedoner/> */}
+
+      {/* <DonationSection/> */}
+
+      {/* <Routedd/> */}
+      {/* <PaymentPage/> */}
+    </>
+  );
 }
 
 export default App;
